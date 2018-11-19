19c95bd61af4bb6279e1d645070f8e155692303f
Caleb <caleb.eliu20@gmail>
2018-11-18 20:10:16 -0500

    se actualizó la bd

--- src/main/resources/bd.sql
+++ src/main/resources/bd.sql
@@ -8,4 +8,12 @@ create table department (
 
 insert into department(name_dep) values ('Java'),('QA'),('AS400'),('Front-end'),('C#'),('Mobile'),('Administrativa');
 
+create table customer(
+	id_cus int auto_increment primary key, 
+    name_cus varchar(50), 
+    last_name_cus varchar(50),
+    id_dep int references department(id_dep)
+);
 
+insert into customer(name_cus, last_name_cus, id_dep) values ('Renato','Salcedo', 1), ('Cesar','Cochachin', 1);
+insert into customer(name_cus, last_name_cus, id_dep) values ('Nelson','', 2);
